6283ea5d6ab000fdafc334cf4bc3a102a10f4d30
Hải <85049064+mountto@users.noreply.github.com>
2026-05-25 14:00:08 +0700

    Create anchor/anchor.c

---
+++ anchor/anchor.c
@@ -0,0 +1,6 @@
+# /**
+# * anchor.c: To get the MAC address of this station's network interface.
+# *
+# * @copyright Copyright (c) 2026, Hoàng Hải <leduchoanghai@yahoo.com>
+# * @license   MIT, http://www.opensource.org/licenses/mit-license.php
+# */
